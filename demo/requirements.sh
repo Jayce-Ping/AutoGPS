3011e7d274b12e69bc0edcdf61de331bf2695038
JaycePing <315229706@qq.com>
2025-06-05 10:02:00 +0800

    Add demo

---
+++ demo/requirements.sh
@@ -0,0 +1,5 @@
+# Install pytorch - https://pytorch.org/get-started/locally/
+pip install torch torchvision torchaudio
+
+# Install other libraries
+pip install matplotlib scipy transformers ultralytics albumentations segmentation_models_pytorch optimum[onnxruntime]
